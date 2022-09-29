a8e4f2359645ef026aa4a4eb5e3464b38d0674a2
B McCool <mccoolcontact@gmail.com>
2022-09-29 02:43:23 -0300

    script start

---
+++ scripts.js
@@ -0,0 +1,28 @@
+var quizQuestion = document.getElementById("");
+var timer = document.getElementById("");
+var startButton = document.getElementById("startButton");
+var questions = document.getElementById
+
+
+// questions
+
+var questions = [
+    {
+        question: "question 1?",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correctAnswer: "answer 1"
+    },
+
+    {
+        question: "question 1?",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correctAnswer: "answer 1"
+    }
+];
+
+
+// start quiz onClick
+
+startButton.onClick = () => {
+    
+};
